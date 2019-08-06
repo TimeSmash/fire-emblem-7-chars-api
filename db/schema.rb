d8942df4a8945237bed4bd753c3561ae65336b90
TimeSmash <TimeSmash@github.com>
2019-08-05 22:37:22 -0400

    Changed database language to Postgres (old was SQLite)

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_31_012135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "gender"
